401efe461513dc2b956194156b42330754e4b493
liemnn <nhamnhuliem@gmail.com>
2026-04-07 17:26:03 +0700

    liemnn

--- custom-addons/ekids_hocsinh/models/hocphi/ban_in/ekids_hocphi_ban_in_model.py
+++ custom-addons/ekids_hocsinh/models/hocphi/ban_in/ekids_hocphi_ban_in_model.py
@@ -40,14 +40,35 @@ class HocPhiBanIn(models.TransientModel):
         default=lambda self: str(date.today().year)
     )
 
+    loai = fields.Selection(
+        [('0', 'In phiếu - Xác nhận thu học phí (phụ huynh ký)')
+            , ('1', 'In phiếu - Học phí(Cơ quan Thuế)')],
+        string='Loai',
+        required=True,
+        default='0'
+
+    )
+
+
+
 
     def get_table_data(self):
 
-        table_data = [['TT','Học sinh', 'Học phí(vnđ)'
-                          ,'Phụ huynh ký xác nhận','Ghi chú'
-                          ]]  # Header
+        dm_bantrus = self.env['ekids.hocphi_dm_thu_bantru'].search(
+            [('coso_id', '=', self.coso_id.id)
+                , ('trangthai', '=', '1')
+                , ('is_hocphi', '=', True)
+
+             ])
+        if self.loai =='0':
+            header = ['TT', 'Học sinh', '1.Học phí (vnđ)','Phụ huynh ký xác nhận', 'Ghi chú']
+        else:
+            header =['TT','Học sinh','1.Học phí (vnđ)','2.Thu khác(vnđ)','Tổng phải đóng =[1]+[2] (vnđ)','Ghi chú','Phụ huynh ký xác nhận',]
+        table_data = [header]  # Header
         thang =self.thang
         nam =self.nam
+
+
 
 
         hocphis = self.env['ekids.hocphi'].search(
@@ -58,19 +79,50 @@ class HocPhiBanIn(models.TransientModel):
              ])
         if hocphis:
             index =1
-            tien =0
             for hocphi in hocphis:
-                table_data = self.get_table_data_by_hocphi(table_data,index,hocphi)
+                table_data = self.get_table_data_by_hocphi(table_data,index,dm_bantrus,hocphi)
                 index =index +1
-                tien+= hocphi.hocphi_phaidong
-            #tong
-            table_data.append([
-                '',
-                'Tổng',
-                string_util.number2string(tien),
-                " ",
-                " "
-            ])
+
+            # thực hiện tính tổng
+            if self.loai == '1':
+                if len(table_data)>1:
+                    index =0
+                    tong_hocphi=0
+                    tong_khac=0
+                    tong_dong=0
+                    for data in table_data:
+                        if index>0:
+                           tong_hocphi += string_util.string2number(data[2])
+                           tong_khac += string_util.string2number(data[3])
+                           tong_dong += string_util.string2number(data[4])
+                        index = index + 1
+
+                    table_data.append([
+                    '',
+                    'Tổng',
+                    string_util.number2string(tong_hocphi),
+                    string_util.number2string(tong_khac),
+                    string_util.number2string(tong_dong),
+                    " ",
+                    " "
+                ])
+            else:
+                if len(table_data) > 1:
+                    index = 0
+                    tong_dong = 0
+                    for data in table_data:
+                        if index > 0:
+                            tong_dong += string_util.string2number(data[2])
+                        index = index + 1
+
+                    table_data.append([
+                        '',
+                        'Tổng',
+                        string_util.number2string(tong_dong),
+                        " ",
+                        " "
+                    ])
+
 
         return table_data
 
@@ -78,15 +130,51 @@ class HocPhiBanIn(models.TransientModel):
 
     #Tinh toán tháng
 
-    def get_table_data_by_hocphi(self,table_data,index,hocphi):
+    def get_table_data_by_hocphi(self,table_data,index,dm_bantrus,hocphi):
         tien =string_util.number2string(hocphi.hocphi_phaidong)
-        table_data.append([
-            str(index),
-            hocphi.hocsinh_id.name,
-            tien,
-            " ",
-            " "
-        ])
+        data =[]
+        data.append(index)
+        data.append(hocphi.hocsinh_id.name)
+        if self.loai == '1':
+            #tính toán thu khác
+            other_desc =""
+            other_tien =0
+            hocphi_tien=0
+            if dm_bantrus:
+                for dm_bantru in dm_bantrus:
+                    hocphi_bantru_ids = hocphi.hocphi_bantru_ids
+                    if hocphi_bantru_ids:
+                        for hocphi_bantru in hocphi_bantru_ids:
+                            if hocphi_bantru.dm_thu_bantru_id.id == dm_bantru.id:
+                                if hocphi.is_giamhocphi_dacthu == True:
+                                    if dm_bantru.is_giam_hocphi ==True:
+                                        other_tien += hocphi_bantru.tien - ((hocphi_bantru.tien / 100) * hocphi.tyle_giamhocphi_bantru)
+                                    else:
+                                        other_tien += hocphi_bantru.tien
+                                else:
+                                    if dm_bantru.is_giam_hocphi ==True:
+                                        other_tien += hocphi_bantru.tien - ((hocphi_bantru.tien / 100) * hocphi.tyle_giamhocphi)
+                                    else:
+                                        other_tien += hocphi_bantru.tien
+                                other_desc += "-" + str(dm_bantru.name) + "=" + string_util.number2string(
+                                            other_tien) + " vnđ \n"
+
+
+                                # them vao tien ban tru
+            hocphi_tien = hocphi.hocphi_phaidong - other_tien
+            data.append(string_util.number2string(hocphi_tien))
+            data.append(string_util.number2string(other_tien))
+            data.append(string_util.number2string(hocphi.hocphi_phaidong))
+            data.append(other_desc)
+            data.append("")
+        else:
+            data.append(string_util.number2string(hocphi.hocphi_phaidong))
+            data.append("")
+            data.append("")
+
+        table_data.append(data)
+
+
         return table_data
 
 
