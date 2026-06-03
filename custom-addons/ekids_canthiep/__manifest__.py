47f42998797f10987e2d7a4ffd23bb5c7a225c19
liemnn <nhamnhuliem@gmail.com>
2026-06-03 14:21:23 +0700

    liemnn

--- custom-addons/ekids_canthiep/__manifest__.py
+++ custom-addons/ekids_canthiep/__manifest__.py
@@ -39,9 +39,8 @@ Long description of module's purpose
         'views/kehoach/ekids_hocsinh_inherit_view.xml',
         'views/kehoach/ekids_kehoach_ketluan_view.xml',
         'views/kehoach/ekids_kehoach_view.xml',
-        'views/kehoach/ekids_kehoach_linhvuc_view.xml',
         'views/kehoach/ekids_kehoach_muctieu_view.xml',
-        'views/kehoach/ekids_kehoach_muctieu_wizard_view.xml',
+
 
         'views/ekids_menu.xml',
 
--- custom-addons/ekids_canthiep/models/kehoach/__init__.py
+++ custom-addons/ekids_canthiep/models/kehoach/__init__.py
@@ -2,9 +2,8 @@
 from . import ekids_hocsinh_inherit_model
 from . import ekids_kehoach_model
 from . import ekids_kehoach_ketluan_model
-from . import ekids_kehoach_linhvuc_model
 from . import ekids_kehoach_muctieu_model
-from . import ekids_kehoach_muctieu_wizard_model
+
 
 
 
