121e55897fc66b7c0676dd28ed61bf4267bab221
liemnn <nhamnhuliem@gmail.com>
2026-06-05 13:44:25 +0700

    liemnn

--- custom-addons/ekids_canthiep/models/kehoach/ekids_kehoach_linhvuc_model.py
+++ custom-addons/ekids_canthiep/models/kehoach/ekids_kehoach_linhvuc_model.py
@@ -14,3 +14,9 @@ class KeHoach2LinhVuc(models.Model):
 
     linhvuc_id = fields.Many2one('ekids.ct_linhvuc', string='Lĩnh vực', required=True, ondelete="cascade")
     tuoi_id = fields.Many2one('ekids.ct_tuoi', string='Độ tuổi', required=True, ondelete="cascade")
+
+    muctieu_ids = fields.Many2many(comodel_name="ekids.ct_muctieu"
+                                           , relation="ekids_kehoach_ct_muctieu4kehoach_rel"
+                                           , column1="kehoach_id"
+                                           , column2="muctieu_id"
+                                           , string="Các mục tiêu cho kế hoạch")
